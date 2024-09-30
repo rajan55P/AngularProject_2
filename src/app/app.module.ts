5b852a6fac143f5ed3160d955d264a84aa09a434
rchanges <rajan.pandey@vtechnolabs.com>
2024-09-30 16:13:07 +0530

    3rd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './features/student/components/student/student.component';
 import { UserComponent } from './features/user/components/user/user.component';
 import {FileProcessingComponent} from './features/file-processing/components/file-processing/file-processing.component';
+import { LoginComponent } from './features/login/login.component';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentComponent,
     UserComponent,
-    FileProcessingComponent
+    FileProcessingComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideClientHydration()
